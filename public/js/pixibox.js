a9833eac99f6609babc71b5e5f09bbe0e5c49066
David Crosby <dcrosby42@gmail.com>
2014-04-03 23:44:41 -0400

    Copied in a PixiJS plus Box2D example

---
+++ public/js/pixibox.js
@@ -0,0 +1,278 @@
+
+/**
+ * @author kozakluke@gmail.com
+ */
+
+MathUtil.RADIANS = Math.PI / 180;
+MathUtil.DEGREES = 180 / Math.PI;
+
+function MathUtil() { }
+
+MathUtil.rndRange = function(min, max)
+{
+    return min + (Math.random() * (max - min));
+}
+
+MathUtil.rndIntRange = function(min, max)
+{
+    return Math.round(MathUtil.rndRange(min, max));
+}
+
+MathUtil.toRadians = function(degrees)
+{
+    return degrees * MathUtil.RADIANS;
+}
+
+MathUtil.toDegrees = function(radians)
+{
+    return radians * MathUtil.DEGREES;
+}
+
+MathUtil.hitTest = function(x1, y1, w1, h1,
+                            x2, y2, w2, h2)
+{
+    if (x1 + w1 > x2)
+        if (x1 < x2 + w2)
+            if (y1 + h1 > y2)
+                if (y1 < y2 + h2)
+                    return true;
+
+    return false;
+}
+
+/**
+ * @author kozakluke@gmail.com
+ */
+ 
+    const STAGE_WIDTH = window.innerWidth, STAGE_HEIGHT = window.innerHeight;
+    const METER = 100;
+    
+    var bodies = [], actors = [];
+    var stage, renderer;
+    var world, mouseJoint;
+    var touchX, touchY;
+    var isBegin;
+    var stats;
+    
+
+		if (!window.requestAnimationFrame) 
+		{
+			window.requestAnimationFrame = (function() {
+				return window.webkitRequestAnimationFrame ||
+				window.mozRequestAnimationFrame ||
+				window.oRequestAnimationFrame ||
+				window.msRequestAnimationFrame ||
+				function(callback) {
+					window.setTimeout(callback, 1000 / 60);
+				};
+			})();
+		}
+		
+
+	
+	function onLoad()
+	{
+        const container = document.createElement("div");
+        document.body.appendChild(container);
+        
+        stats = new Stats();
+        container.appendChild(stats.domElement);
+        stats.domElement.style.position = "absolute";
+        
+        stage = new PIXI.Stage(0xDDDDDD, true);
+        
+        renderer = PIXI.autoDetectRenderer(STAGE_WIDTH, STAGE_HEIGHT, undefined, false);
+        document.body.appendChild(renderer.view);
+        
+        const loader = new PIXI.AssetLoader(["pixibox_assets/ball.png",
+                                             "pixibox_assets/box.jpg"]);
+        loader.onComplete = onLoadAssets;
+        loader.load();
+	}
+
+		window.onload = onLoad;
+    
+    function onLoadAssets()
+    {
+        world = new Box2D.Dynamics.b2World(new Box2D.Common.Math.b2Vec2(0, 10),  true);
+        
+        const polyFixture = new Box2D.Dynamics.b2FixtureDef();
+        polyFixture.shape = new Box2D.Collision.Shapes.b2PolygonShape();
+        polyFixture.density = 1;
+        
+        const circleFixture	= new Box2D.Dynamics.b2FixtureDef();
+        circleFixture.shape	= new Box2D.Collision.Shapes.b2CircleShape();
+        circleFixture.density = 1;
+        circleFixture.restitution = 0.7;
+        
+        const bodyDef = new Box2D.Dynamics.b2BodyDef();
+        bodyDef.type = Box2D.Dynamics.b2Body.b2_staticBody;
+        
+        //down
+        polyFixture.shape.SetAsBox(10, 1);
+        bodyDef.position.Set(9, STAGE_HEIGHT / METER + 1);
+        world.CreateBody(bodyDef).CreateFixture(polyFixture);
+        
+        //left
+        polyFixture.shape.SetAsBox(1, 100);
+        bodyDef.position.Set(-1, 0);
+        world.CreateBody(bodyDef).CreateFixture(polyFixture);
+        
+        //right
+        bodyDef.position.Set(STAGE_WIDTH / METER + 1, 0);
+        world.CreateBody(bodyDef).CreateFixture(polyFixture);
+        bodyDef.type = Box2D.Dynamics.b2Body.b2_dynamicBody;
+        
+        for (var i = 0; i < 40; i++)
+        {
+            bodyDef.position.Set(MathUtil.rndRange(0, STAGE_WIDTH) / METER, -MathUtil.rndRange(50, 5000) / METER);
+            var body = world.CreateBody(bodyDef);
+            var s;
+            if (Math.random() > 0.5)
+            {
+                s = MathUtil.rndRange(70, 100);
+                circleFixture.shape.SetRadius(s / 2 / METER);
+                body.CreateFixture(circleFixture);
+                bodies.push(body);
+                
+                var ball = new PIXI.Sprite(PIXI.Texture.fromFrame("pixibox_assets/ball.png"));
+                stage.addChild(ball);
+                ball.i = i;
+                ball.anchor.x = ball.anchor.y = 0.5;
+                ball.scale.x = ball.scale.y = s / 100;
+                
+                actors[actors.length] = ball;
+            }
+            else
+            {
+                s = MathUtil.rndRange(50, 100);
+                polyFixture.shape.SetAsBox(s / 2 / METER, s / 2 / METER);
+                body.CreateFixture(polyFixture);
+                bodies.push(body);
+                
+                var box = new PIXI.Sprite(PIXI.Texture.fromFrame("pixibox_assets/box.jpg"));
+                stage.addChild(box);
+                box.i = i;
+                box.anchor.x = box.anchor.y = 0.5;
+                box.scale.x = s / 100;
+                box.scale.y = s / 100;
+                
+                actors[actors.length] = box;
+            }
+        }
+        
+        document.addEventListener("mousedown", function(event) {
+            isBegin = true;
+            onMove(event);
+            document.addEventListener("mousemove", onMove, true);
+        }, true);
+        
+        document.addEventListener("mouseup", function(event) {
+            document.removeEventListener("mousemove", onMove, true);
+            isBegin = false;
+            touchX = undefined;
+            touchY = undefined;
+        }, true);
+        
+        renderer.view.addEventListener("touchstart", function(event) {
+            isBegin = true;
+            onMove(event);
+            renderer.view.addEventListener("touchmove", onMove, true);
+        }, true);
+        
+        renderer.view.addEventListener("touchend", function(event) {
+            renderer.view.removeEventListener("touchmove", onMove, true);
+            isBegin = false;
+            touchX = undefined;
+            touchY = undefined;
+        }, true);
+        
+        update();
+    }
+    
+    function getBodyAtMouse()
+    {
+        const mousePos = new Box2D.Common.Math.b2Vec2(touchX, touchY);
+        const aabb = new Box2D.Collision.b2AABB();
+        aabb.lowerBound.Set(touchX - 0.001, touchY - 0.001);
+        aabb.upperBound.Set(touchX + 0.001, touchY + 0.001);
+        
+        var body;
+        world.QueryAABB(
+            function (fixture)
+            {
+                if(fixture.GetBody().GetType() != Box2D.Dynamics.b2BodyDef.b2_staticBody)
+                {
+                    if(fixture.GetShape().TestPoint(fixture.GetBody().GetTransform(), mousePos)) {
+                        body = fixture.GetBody();
+                        return false;
+                    }
+                }
+                return true;
+            }, aabb);
+        
+        return body;
+    }
+    
+    function onMove(event)
+    {
+        if (event["changedTouches"])
+        {
+            var touche = event["changedTouches"][0];
+            touchX = touche.pageX / METER;
+            touchY = touche.pageY / METER;
+        }
+        else {
+            touchX = event.clientX / METER;
+            touchY = event.clientY / METER;
+        }
+    }
+    
+	function update()
+	{
+		requestAnimationFrame(update);
+        
+        if(isBegin && !mouseJoint)
+        {
+            const dragBody = getBodyAtMouse();
+            if(dragBody)
+			{
+                const jointDef = new Box2D.Dynamics.Joints.b2MouseJointDef();
+                jointDef.bodyA = world.GetGroundBody();
+                jointDef.bodyB = dragBody;
+                jointDef.target.Set(touchX, touchY);
+                jointDef.collideConnected = true;
+                jointDef.maxForce = 300.0 * dragBody.GetMass();
+                mouseJoint = world.CreateJoint(jointDef);
+                dragBody.SetAwake(true);
+            }
+        }
+        
+        if(mouseJoint)
+        {
+            if(isBegin)
+                mouseJoint.SetTarget(new Box2D.Common.Math.b2Vec2(touchX, touchY));
+            else {
+                world.DestroyJoint(mouseJoint);
+                mouseJoint = null;
+            }
+        }
+        
+        world.Step(1 / 60,  3,  3);
+        world.ClearForces();
+        
+        const n = actors.length;
+        for (var i = 0; i < n; i++)
+        {
+            var body  = bodies[i];
+            var actor = actors[i];
+            var position = body.GetPosition();
+            actor.position.x = position.x * 100;
+            actor.position.y = position.y * 100;
+            actor.rotation = body.GetAngle();
+        }
+        
+        renderer.render(stage);
+        stats.update();
+	}
+
